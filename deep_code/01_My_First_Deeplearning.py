64c9d42808e86c3b83f36947f0ae310dcbe93e14
hermes7308 <hermes7308@gmail.com>
2020-05-09 21:45:16 -0400

    01_My_First_Deeplearning.py: 변경 메소드 수정

--- deep_code/01_My_First_Deeplearning.py
+++ deep_code/01_My_First_Deeplearning.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 # 코드 내부에 한글을 사용가능 하게 해주는 부분입니다.
 
-# 딥러닝을 구동하는 데 필요한 케라스 함수를 불러옵니다.
-from keras.models import Sequential
-from keras.layers import Dense
-
 # 필요한 라이브러리를 불러옵니다.
 import numpy
 import tensorflow as tf
+from keras.layers import Dense
+# 딥러닝을 구동하는 데 필요한 케라스 함수를 불러옵니다.
+from keras.models import Sequential
 
 # 실행할 때마다 같은 결과를 출력하기 위해 설정하는 부분입니다.
 seed = 0
@@ -18,8 +17,8 @@ tf.random.set_seed(seed)
 Data_set = numpy.loadtxt("../dataset/ThoraricSurgery.csv", delimiter=",")
 
 # 환자의 기록과 수술 결과를 X와 Y로 구분하여 저장합니다.
-X = Data_set[:,0:17]
-Y = Data_set[:,17]
+X = Data_set[:, 0:17]
+Y = Data_set[:, 17]
 
 # 딥러닝 구조를 결정합니다(모델을 설정하고 실행하는 부분입니다).
 model = Sequential()
@@ -32,5 +31,3 @@ model.fit(X, Y, epochs=30, batch_size=10)
 
 # 결과를 출력합니다.
 print("\n Accuracy: %.4f" % (model.evaluate(X, Y)[1]))
-
-
