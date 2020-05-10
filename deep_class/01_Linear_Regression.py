ad8cd94675ef5daf9a9a8f8e33d696f0810728e9
hermes7308 <hermes7308@gmail.com>
2020-05-09 22:07:47 -0400

    01_Linear_Regression.py: 정렬

--- deep_class/01_Linear_Regression.py
+++ deep_class/01_Linear_Regression.py
@@ -2,8 +2,8 @@
 import numpy as np
 
 # x 값과 y값
-x=[2, 4, 6, 8]
-y=[81, 93, 91, 97]
+x = [2, 4, 6, 8]
+y = [81, 93, 91, 97]
 
 # x와 y의 평균값
 mx = np.mean(x)
@@ -12,7 +12,8 @@ print("x의 평균값:", mx)
 print("y의 평균값:", my)
 
 # 기울기 공식의 분모
-divisor = sum([(mx - i)**2 for i in x])
+divisor = sum([(mx - i) ** 2 for i in x])
+
 
 # 기울기 공식의 분자
 def top(x, mx, y, my):
@@ -20,6 +21,8 @@ def top(x, mx, y, my):
     for i in range(len(x)):
         d += (x[i] - mx) * (y[i] - my)
     return d
+
+
 dividend = top(x, mx, y, my)
 
 print("분모:", divisor)
@@ -27,7 +30,7 @@ print("분자:", dividend)
 
 # 기울기와 y 절편 구하기
 a = dividend / divisor
-b = my - (mx*a)
+b = my - (mx * a)
 
 # 출력으로 확인
 print("기울기 a =", a)
