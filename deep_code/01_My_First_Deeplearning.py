364e6da9f4638903869595e8d0774d7719e34ae6
hermes7308 <hermes7308@gmail.com>
2020-05-09 21:46:23 -0400

    01_My_First_Deeplearning.py: 변경 메소드 수정

--- deep_code/01_My_First_Deeplearning.py
+++ deep_code/01_My_First_Deeplearning.py
@@ -4,9 +4,9 @@
 # 필요한 라이브러리를 불러옵니다.
 import numpy
 import tensorflow as tf
-from keras.layers import Dense
 # 딥러닝을 구동하는 데 필요한 케라스 함수를 불러옵니다.
 from keras.models import Sequential
+from keras.layers import Dense
 
 # 실행할 때마다 같은 결과를 출력하기 위해 설정하는 부분입니다.
 seed = 0
